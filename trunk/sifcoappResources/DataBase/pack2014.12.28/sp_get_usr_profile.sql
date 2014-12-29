7a3549cc018742b91d2b2c9e74cf47524bf46196
rutilio.iraheta <rutilio.iraheta@97f14a1b-7afe-1931-fe02-c6aa605fdd89>
2014-12-29 04:00:16 +0000

    pack 2014.12.28 security

---
+++ trunk/sifcoappResources/DataBase/pack2014.12.28/sp_get_usr_profile.sql
@@ -0,0 +1,26 @@
+﻿-- Function: sp_get_usr_profile(integer)
+
+-- DROP FUNCTION sp_get_usr_profile(integer);
+
+CREATE OR REPLACE FUNCTION sp_get_usr_profile(_profilecode integer)
+  RETURNS SETOF refcursor AS
+$BODY$
+DECLARE
+ref1 refcursor;
+
+BEGIN
+OPEN ref1 for SELECT det.profilecode, profiledetcode, profiledetparent, profiledetdesc, 
+       profiledeturl, profiledeticon, prof.profilename
+  FROM adm_usr2_profile_det det, adm_usr1_profile prof
+  where det.profilecode=prof.profilecode
+  and det.profilecode=_profilecode;
+
+return NEXT ref1; 
+
+END;
+    $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION sp_get_usr_profile(integer)
+  OWNER TO postgres;
